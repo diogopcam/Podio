00aa773e77bebfa42db0aec7049409b69cdac356
diogpcam <diogpcam@gmail.com>
2024-08-05 20:37:46 -0300

    Commit inicial do projeto. MongoDB conectado.

---
+++ src/constants/Fonts.js
@@ -0,0 +1,4 @@
+// fonts.js
+export const QuicksandFont = `
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap');
+`;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Verificar se as variáveis de ambiente estão corretas
+console.log(`MONGODB_USER: ${process.env.MONGODB_USER}`);
+console.log(`MONGODB_PASSWORD: ${process.env.MONGODB_PASSWORD}`);
+console.log(`MONGODB_CLUSTER: ${process.env.MONGODB_CLUSTER}`);
+console.log(`MONGODB_APP_NAME: ${process.env.MONGODB_APP_NAME}`);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGODB_APP_NAME}`;
+
+console.log(`Connection URI: ${uri}`);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
