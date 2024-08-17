b868c6febdaf312a75d59e5ad9e41945a03c815b
diogpcam <diogpcam@gmail.com>
2024-08-17 17:05:00 -0300

    Insercao da fonte Quicksand.

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -5,12 +5,13 @@ import Logo from './images/podio-logo.svg'
 function HomeScreen(){
     return(
         <div className='FullScreen'>
+            <div> <p></p></div>
             <div className="LogoText">
                 <img className="Logo" src={Logo}/>
                 <p className='Text'> Apoie os atletas do<br/>nosso país </p>
             </div>
             <div className='Description'> 
-                <p> O PODIO é um projeto sem fins lucrativos que objetiva proporcionar uma central de informações segura e unificada sobre os atletas do nosso país. </p>
+                <p> O PODIO é um projeto sem fins lucrativos que objetiva proporcionar uma central de informações segura e unificada sobre os<br/>atletas do nosso país. </p>
             </div>
         </div>
     )
--- src/constants/Fonts.js
+++
@@ -1,4 +0,0 @@
-// fonts.js
-export const QuicksandFont = `
-  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap');
-`;
